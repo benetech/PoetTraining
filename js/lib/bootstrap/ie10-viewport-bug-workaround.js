c5837f8d8485572fad8e5b2146aefea5304fdc34
nirupa-kumar <nirupa.kumar@gmail.com>
2016-02-11 00:05:45 -0800

    IMG-939:Code replication to backbone

---
+++ js/lib/bootstrap/ie10-viewport-bug-workaround.js
@@ -0,0 +1,26 @@
+/**
+ * Created by nirupak on 2/8/16.
+ */
+/*! Reference : https://github.com/twbs/bootstrap/blob/master/docs/assets/js/ie10-viewport-bug-workaround.js
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+
+// See the Getting Started docs for more information:
+// http://getbootstrap.com/getting-started/#support-ie10-width
+
+(function () {
+    'use strict';
+
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement('style')
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                '@-ms-viewport{width:auto!important}'
+            )
+        )
+        document.querySelector('head').appendChild(msViewportStyle)
+    }
+
+})();
