31371f4bad222c528079591d64ccc4226835b12e
Jason Schwab <jasons@benetech.org>
2017-03-13 10:14:02 -0700

    Lots of random changes from Sue-Ann

--- scripts/how.js
+++ scripts/how.js
@@ -1,4 +1,4 @@
-var how01 = [{	"name":"Example 01",
+var how01 = [{	"name":"Context is Key",
     				"imageURL": "images/how_example01.jpg",
     				"caption":"",
     				"guidelines": "<ul> \
@@ -16,114 +16,114 @@ var how01 = [{	"name":"Example 01",
     				"context": "This illustration is found in the chapter summary of a biology textbook with the following question: Which of the following is not typically described as one of the four main stages of Mitosis? Where, in mitosis, is that process likely to be covered? ",
     				"questions": [{
 	    				"question":"Mitosis",
-	    				"answer":"Nice try. This description is too high-level and does not provide sufficient details about the embedded content required to answer the question.",
+	    				"answer":"<strong>Try again</strong>. This description is too high-level and does not provide sufficient details about the embedded content required to answer the question.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"Mitosis stages: Prophase, Prometaphase, Metaphase, Anaphase, and Telophase",
-	    				"answer":"Good choice. The description starts with the high-level concept, and then delves into the details of the illustration.",
+	    				"answer":"<strong>Correct!</strong> The description starts with the high-level concept, and then delves into the details of the illustration.",
 	    				"correct":"yes"
 	    				},{
 	    				"question":"Cells progressing through the stages of mitosis: Prophase is when chromatin become condensed; Prometaphase; Metaphase is the longest stage; Anaphase is the shortage stage; and Telophase is when two daughter nuclei form in the cell.",
-	    				"answer":"Nice try. If this series was presented as part of the main text rather than a chapter summary exercise, the brief descriptions associated with the various stages of mitosis could have been helpful to the reader if the information was found in the surrounding text. However, in this context, the details are excessive and leading. In addition, “Prometaphase” is presented in a different way than the other stages. This calls attention to it, and potentially gives away part of the answer.",
+	    				"answer":"<strong>Try again</strong>. If this series was presented as part of the main text rather than a chapter summary exercise, the brief descriptions associated with the various stages of mitosis could have been helpful to the reader if the information was found in the surrounding text. However, in this context, the details are excessive and leading. In addition, “Prometaphase” is presented in a different way than the other stages. This calls attention to it, and potentially gives away part of the answer.",
 	    				"correct":"no"
 	    				}]
 		    			}];
-var how02 = [{	"name":"Example 02",
+var how02 = [{	"name":"Consider Your Audience",
     				"imageURL": "images/how_example02.jpg",
     				"caption":"",
     				"guidelines": "<ul><li>Know your target reader (e.g. age, culture, subject-matter expertise).</li><li>Use vocabulary and phrases appropriate for the reader.</li><li>Reference examples and details the reader will understand (this includes objects and attributes used in the description).</li></ul>",
     				"context": "Describe the photo of the Louvre designed by architect I.M. Pei to a young child.",
     				"questions": [{
 	    				"question":"This is a photograph of the Louvre Museum in France at night. The entrance to the museum is a large pyramid made out of glass.",
-	    				"answer":"Good choice. This description uses language and introduces shapes appropriate for a young child.",
+	    				"answer":"<strong>Correct!</strong> This description uses language and introduces shapes appropriate for a young child.",
 	    				"correct":"yes"
 	    				},{
 	    				"question":"This is a night photograph of the Louvre Pyramids in front of the Louvre Palace. The juxtaposition of the architectural structures highlight I.M. Pei’s proclivity for combining modern structures and classical forms. ",
-	    				"answer":"Nice try. The description is accurate, but the use of vocabulary such as “juxtaposition” and “proclivity,” is far too complex for a young child. This might have been an appropriate description if it had been written for a more mature audience.",
+	    				"answer":"<strong>Try again</strong>. The description is accurate, but the use of vocabulary such as “juxtaposition” and “proclivity,” is far too complex for a young child. This might have been an appropriate description if it had been written for a more mature audience.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"This is a photograph of the Louvre Pyramids was taken at night. The low-angle shot, paired with indirect lighting of key architectural elements, makes for a dramatic composition.",
-	    				"answer":"Nice try. This description is accurate and appropriate for a photography aficionado, but provides too much detail about composition and design for the average child or adult.",
+	    				"answer":"<strong>Try again</strong>. This description is accurate and appropriate for a photography aficionado, but provides too much detail about composition and design for the average child or adult.",
 	    				"correct":"no"
 	    				}]
     			}];
 
-var how03 = [{	"name":"Example 03",
+var how03 = [{	"name":"Be Concise",
     				"imageURL": "images/how_example03.jpg",
     				"caption": "Figure 1.1: The orbiting paths of the eight planets in our solar system around the sun - Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus and Neptune.",
     				"guidelines": "<ul><li>More is NOT better – be succinct. </li><li>Don’t repeat information presented in the main or adjacent texts. Instead, direct readers to existing descriptions, when available (e.g. captions). </li><li>Include color only when it is significant (e.g. arbitrary colors assigned for elements of bar graphs and line charts need not be specified). </li><li>Avoid introducing new concepts or terms.</li></ul>",
     				"context": "This photograph and caption are found in a fifth grade science textbook in a section about our solar system.",
     				"questions": [{
 	    				"question":"Figure 1.1",
-	    				"answer":"Good choice. The accompanying caption provides sufficient information about the photograph, so readers should be directed to the existing text.",
+	    				"answer":"<strong>Correct!</strong> The accompanying caption provides sufficient information about the photograph, so readers should be directed to the existing text.",
 	    				"correct":"yes"
 	    				},{
 	    				"question":"The sun and planets make up our galaxy. The orbiting paths are represented by a dark-orange line for Mercury, yellow line for Venus, green line for Earth, red line for Mars, light-orange line for Jupiter, pastel-yellow line for Saturn, light-blue line for Uranus, and dark-blue line for Neptune.",
-	    				"answer":"Nice try. This description is overly detailed and repeats information introduced in the accompanying caption. The additional details about the color of the orbiting paths are excessive and not necessary for understanding the concept of orbital paths.",
+	    				"answer":"<strong>Try again</strong>. This description is overly detailed and repeats information introduced in the accompanying caption. The additional details about the color of the orbiting paths are excessive and not necessary for understanding the concept of orbital paths.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"The sun and planets are in order of their distance from the sun. Pluto, previously known as the ninth planet in our solar system, is now considered a dwarf planet. This declassification took place in August of 2006, and Pluto’s orbit is represented by a purple line.",
-	    				"answer":"Nice try. This description is accurate and presents facts some readers may find interesting, but new information should not be introduced through image descriptions.",
+	    				"answer":"<strong>Try again</strong>. This description is accurate and presents facts some readers may find interesting, but new information should not be introduced through image descriptions.",
 	    				"correct":"no"
 	    				}]
     			}];
 
-var how04 = [{	"name":"Example 04",
+var how04 = [{	"name":"Be Objective",
     				"imageURL": "images/how_example04.jpg",
     				"caption": "",
     				"guidelines": "<ul><li>Describe only what you see – physical appearances and actions rather than emotions and possible intentions. </li><li>Don't interpret or analyze the material. Instead, allow readers to form their own opinions. </li><li>Don’t omit uncomfortable or controversial content, such as images associated with politics, religion, or sex.</li></ul>",
     				"context": "This photograph is found on a geology website discussing various events that lead to the acid erosion of rock formations over time.",
     				"questions": [{
 	    				"question":"A close-up of a puffin, which is a fluffy, black-and-white bird with bright orange feet and a colorful beak comprised by wide bands of red, yellow, and black stripes. ",
-						"answer":"Nice try. This accurately identifies and describes the bird in the image, but omits detail about the bird defecating, which is one reason for acid erosion.",
+						"answer":"<strong>Try again</strong>. This accurately identifies and describes the bird in the image, but omits detail about the bird defecating, which is one reason for acid erosion.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"A cute puffin doing his business on an earthy, red rock.",
-						"answer":"Nice try. Describing the bird as “cute,” assigns subjectivity to the object in the photo. Furthermore, “doing his business” is ambiguous and could be considered censorship of an action the writer is uncomfortable describing.",
+						"answer":"<strong>Try again</strong>. Describing the bird as “cute,” assigns subjectivity to the object in the photo. Furthermore, “doing his business” is ambiguous and could be considered censorship of an action the writer is uncomfortable describing.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"A puffin bird with white liquid projecting from its tail end stands on a rocky mound covered by white excrements.",
-						"answer":"Good choice. This description quickly identifies the puffin as a bird for those who may not be familiar. It also states the bird’s current status and objectively presents defecation as a likely cause of acid erosion.",
+						"answer":"<strong>Correct!</strong> This description quickly identifies the puffin as a bird for those who may not be familiar. It also states the bird’s current status and objectively presents defecation as a likely cause of acid erosion.",
 	    				"correct":"yes"
 	    				}]
     			}];
     			
-var how05 = [{	"name":"Example 05",
+var how05 = [{	"name":"General to Specific",
     				"imageURL": "images/how_example05.jpg",
     				"caption": "",
     				"guidelines": "<ul><li>Start with high-level context, and then drill down to details that enhance understanding. This provides the reader with options about how much information to read.</li><li>Segment content into logical, digestible chunks.</li></ul>",
     				"context": "This map is found in an art history book. The section introduces the concept of using recycled materials to produce art.",
     				"questions": [{
 	    				"question":"Artistic depiction of a US map.",
-						"answer":"Nice try. This description is too simplistic and lacks details about why the map is considered artistic.",
+						"answer":"<strong>Try again</strong>. This description is too simplistic and lacks details about why the map is considered artistic.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"A map of the United States made out of recycled license plates. This “recycled map” is about five feet wide, with each state represented by license plate cutouts from the respective state. Large states such as Texas and California are made of one or more colorful metal license plates while smaller New England states are represented by just a few inches of their license plates.",
-						"answer":"Good choice. This accurately starts with a high-level context about the recycled nature of the map, and then provides readers with the option to drill-down to learn more about the collage.",
+						"answer":"<strong>Correct!</strong> This accurately starts with a high-level context about the recycled nature of the map, and then provides readers with the option to drill-down to learn more about the collage.",
 	    				"correct":"yes"
 	    				},{
 	    				"question":"California is made from two white license plates, each including a setting sun. Arizona is made from one red license plate with an off-white cactus on it. (The description would continue to describe each individual license plate.) This is an artistic depiction of a map of the United States.",
-						"answer":"Nice try. The description starts by diving into the details and fails to orient the reader with a higher-level overview. The details provided in the description about each state’s specific license plate is also excessive and is likely to overwhelm, rather than facilitate reader comprehension.",
+						"answer":"<strong>Try again</strong>. The description starts by diving into the details and fails to orient the reader with a higher-level overview. The details provided in the description about each state’s specific license plate is also excessive and is likely to overwhelm, rather than facilitate reader comprehension.",
 	    				"correct":"no"
 	    				}]
     			}];
     			
-var how06 = [{	"name":"Example 06",
+var how06 = [{	"name":"Tone & Language",
     				"imageURL": "images/how_example06.jpg",
     				"caption": "",
     				"guidelines": "<ul><li>Use active verbs in the present tense.</li><li>Check spelling, grammar, and punctuation. Note: Sometimes it is acceptable to break traditional grammar rules for brevity and clarity. However, it is important to be consistent in this practice.</li><li>Apply the same writing style and terminology as the surrounding text.</li><li>Write out abbreviations and symbols to ensure proper pronunciation by screen readers.</li><li>Use descriptive vocabulary that adds meaning (e.g. 'map' instead of 'image').</li></ul>",
     				"context": "This photograph of the Grand Canyon was taken in early September, which is Arizona’s monsoon season. The description and image appear in a book about national parks in the United States.",
     				"questions": [{
 	    				"question":"An auspicious photograph of the Grand Canyon. A blue sky peaks through grey clouds, casting an glow across Arizona.",
-						"answer":"Nice try. Avoid vocabulary that you might not apply correctly (here, the use of “auspicious” is questionable). Also, use proper grammar (“a” versus “an”), and do not overextend the scope of a description. For example, assuming “a glow” has been cast across all of Arizona rather than just the portion of the Grand Canyon shown in the photograph.",
+						"answer":"<strong>Try again</strong>. Avoid vocabulary that you might not apply correctly (here, the use of “auspicious” is questionable). Also, use proper grammar (“a” versus “an”), and do not overextend the scope of a description. For example, assuming “a glow” has been cast across all of Arizona rather than just the portion of the Grand Canyon shown in the photograph.",
 	    				"correct":"no"
 	    				},{
 	    				"question":"An image of a blue sky peeking through grey storm clouds over the Grand Canyon in early Sept.",
-						"answer":"Nice try. Apply adjectives that add meaning – for example, “photograph” over the more generic, “image.” This option provides the right level of information about the photograph, but abbreviations should be avoided in descriptions. Here, truncating the month would cause a screen reader to announce the date as “Sept” rather than the full reading, “September.”",
+						"answer":"<strong>Try again</strong>. Apply adjectives that add meaning – for example, “photograph” over the more generic, “image.” This option provides the right level of information about the photograph, but abbreviations should be avoided in descriptions. Here, truncating the month would cause a screen reader to announce the date as “Sept” rather than the full reading, “September.”",
 	    				"correct":"no"
 	    				},{
 	    				"question":"A photograph of a blue sky peeking through grey storm clouds over the Grand Canyon in early September.",
-						"answer":"Good choice. This accurately describes the image and captures the tone of the scene. The description also avoids the use of abbreviations to ensure proper reading by screen readers. It is also an incomplete sentence, but it is clear and brief, which ultimately benefits the student.  Finally, after hearing about the “violent” storm in the text that precedes the image, the use of “peeking” to describe the blue sky creates the right tone.",
+						"answer":"<strong>Correct!</strong> This accurately describes the image and captures the tone of the scene. The description also avoids the use of abbreviations to ensure proper reading by screen readers. It is also an incomplete sentence, but it is clear and brief, which ultimately benefits the student.  Finally, after hearing about the “violent” storm in the text that precedes the image, the use of “peeking” to describe the blue sky creates the right tone.",
 	    				"correct":"yes"
 	    				}]
     			}];
